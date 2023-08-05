6c8ffeb8911a7c3054d4b357dceff86b97ee3139
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-08-04 23:04:00 -0300

    Basic Implementation

---
+++ README.md
@@ -0,0 +1,31 @@
+## Rock Paper Scissors Pro
+
+Welcome to the Rock-Paper-Scissors game – a classic hand game implemented in SwiftUI!
+
+### How to Play
+
+1. Clone the repository to your local machine.
+2. Open the project using Xcode or any SwiftUI-compatible IDE.
+3. Build and run the app on a simulator or a physical device.
+
+### Gameplay
+
+The objective is simple: beat the computer by making the right moves! The game is set for a maximum of 10 moves. You will be presented with the computer's move, and you have to choose whether you want to **Win** or **Lose** against it. Select your move by tapping on the corresponding button – **Rock**, **Paper**, or **Scissors**.
+
+### Scoring
+
+- If you choose the move that beats the computer's move, you gain a point.
+- If you choose the move that loses to the computer's move, you lose a point.
+- In case of a tie, neither player gains or loses a point.
+
+### Keep an Eye on the Clock!
+
+With only 10 moves allowed, you'll need to make strategic choices to maximize your score. Think fast, but think wisely!
+
+### Contribute
+
+Found a bug? Want to add more features? Contributions are always welcome! Feel free to open issues, submit pull requests, and make this game even better.
+
+### Have Fun!
+
+Enjoy the classic RockPaperScissors game and test your wits against the computer. May the odds be in your favor! 😄👊🤚
--- RockPaperScissors/ContentView.swift
+++ RockPaperScissors/ContentView.swift
@@ -7,15 +7,106 @@
 
 import SwiftUI
 
+enum Move {
+    case rock, paper, scissors
+    
+    static func getString(for move: Move) -> String {
+        switch move {
+        case .rock:     return "Rock"
+        case .paper:    return "Paper"
+        case .scissors: return "Scissors"
+        }
+    }
+    
+    static func getRandomMove() -> Move {
+        return [Move.rock, .paper, .scissors].randomElement()!
+    }
+}
+
+enum MatchResult {
+    case lose, win, tie
+}
+
 struct ContentView: View {
+    
+    let maximumMoves = 10
+    @State private var currentNumberOfMoves = 0
+    
+    @State private var currentMove = Move.getRandomMove()
+    @State private var playerHasToWin = Bool.random() // Toggles between turns.
+    @State private var playerScore = 0
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Spacer()
+            
+            Text("Score: \(playerScore)")
+            
+            Spacer()
+            
+            VStack (spacing: 10) {
+                Text("\(playerHasToWin ? "Win": "Lose")")
+                Text(Move.getString(for: currentMove))
+            }
+            
+            Spacer()
+            
+            VStack (spacing: 20) {
+                Button(Move.getString(for: .rock))      { buttonPressed(for: .rock) }
+                Button(Move.getString(for: .paper))     { buttonPressed(for: .paper) }
+                Button(Move.getString(for: .scissors))  { buttonPressed(for: .scissors) }
+            }
+            
+            Spacer()
         }
-        .padding()
+    }
+    
+    private func buttonPressed(for playerMove: Move) {
+        
+        // Check result.
+        let matchResult = ContentView.userMatchResult(computerMove: currentMove, playerMove: playerMove, playerHasToWin: playerHasToWin)
+        
+        // Update score.
+        if matchResult == .win {
+            playerScore += 1
+        } else if matchResult == .lose {
+            playerScore -= 1
+        }
+        
+        // Keep track of current number of moves.
+        currentNumberOfMoves += 1
+        
+        //TODO: Handle more than maximum number of moves.
+        
+        // Next move.
+        currentMove = Move.getRandomMove()
+        playerHasToWin.toggle()
+    }
+    
+    private static func userMatchResultWhenUserHasToWin(computerMove: Move, playerMove: Move) -> MatchResult {
+        if computerMove == .rock        && playerMove == .paper       { return .win}
+        if computerMove == .rock        && playerMove == .scissors    { return .lose}
+        
+        if computerMove == .paper       && playerMove == .rock        { return .lose}
+        if computerMove == .paper       && playerMove == .scissors    { return .win}
+        
+        if computerMove == .scissors    && playerMove == .rock        { return .win}
+        if computerMove == .scissors    && playerMove == .paper       { return .lose}
+        
+        return .tie // Since, both of their moves are same at this point.
+    }
+    
+    private static func userMatchResult(computerMove: Move, playerMove: Move, playerHasToWin: Bool) -> MatchResult {
+        let matchResultWhenUserHasToWin = userMatchResultWhenUserHasToWin(computerMove: computerMove, playerMove: playerMove)
+        
+        if playerHasToWin {
+            return matchResultWhenUserHasToWin
+        }
+        
+        // User has to lose.
+        if matchResultWhenUserHasToWin == .win { return .lose }
+        if matchResultWhenUserHasToWin == .lose { return .win }
+        return .tie // Since, a tie is always a tie.
     }
 }
 
