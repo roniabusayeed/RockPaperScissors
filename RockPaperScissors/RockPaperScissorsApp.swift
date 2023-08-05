ff1da33431391215f51805d18798b9e725bbb343
Abu Sayeed Roni <roni.abusayeed@dal.ca>
2023-08-04 21:30:31 -0300

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Abu Sayeed Roni on 2023-08-04.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
